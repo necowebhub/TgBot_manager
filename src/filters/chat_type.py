2455b3ceed298e9256e55f3fd618eb9dbfd9b1dd
nekohub <135654626+necowebhub@users.noreply.github.com>
2026-01-19 22:09:19 +0200

    Add isPrivateChannelChat filter for channel chats

--- src/filters/chat_type.py
+++ src/filters/chat_type.py
@@ -4,3 +4,7 @@ from aiogram.types import Message
 class IsPrivateChat(BaseFilter):
     async def __call__(self, message: Message) -> bool:
         return message.chat.type == "private"
+
+class isPrivateChannelChat(BaseFilter):
+    async def __call__(self, message: Message) -> bool:
+        return message.chat.type == "channel"
